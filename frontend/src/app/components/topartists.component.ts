113357fd19165aab641b7ae98761d2f8b11f3525
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-12-07 19:55:14 +0100

    Add more styles to artistsSection

--- frontend/src/app/components/topartists.component.ts
+++ frontend/src/app/components/topartists.component.ts
@@ -9,8 +9,8 @@ import { DashboardComponent } from './dashboard.component';
     template: `
     <section class="flex flex-col items-center w-[100%] h-[100%]">
         <h2 class="text-5xl font-bold border-b-2 border-b-black">TopArtists</h2>
-        <ol class="list-decimal">
-            <li *ngFor="let artist of artists.items">
+        <ol class="list-decimal w-[80%]">
+            <li class="text-xl w-[100%] bg-black/5 m-2 p-1" *ngFor="let artist of artists.items">
                 {{artist.name}}
             </li>
         </ol>
