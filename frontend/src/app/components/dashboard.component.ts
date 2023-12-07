e2343c46ad19d004703b7ee23d82a78080101a7b
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-12-07 20:08:46 +0100

    Add back button

--- frontend/src/app/components/dashboard.component.ts
+++ frontend/src/app/components/dashboard.component.ts
@@ -15,7 +15,7 @@ import { TopArtistsComponent } from './topartists.component';
        </div>
        <ng-template #infoSection>
             <div class="w-[30%] border-r-2 border-r-black p-5 flex flex-col items-center justify-start gap-[10%]">
-                <h2 class="text-5xl font-bold border-b-2 border-b-black">DASHBOARD</h2>
+                <h2 class="text-5xl font-bold border-b-2 border-b-black w-[100%]">DASHBOARD</h2>
                 <img class="rounded-3xl w-[55%] " src={{info.images[1].url}} alt="avatar">
                 <div class="text-2xl">
                 <p>name: {{info.display_name}}</p>
@@ -23,7 +23,15 @@ import { TopArtistsComponent } from './topartists.component';
                 <p>followers: {{info.followers.total}}</p>
                 </div>
             </div>
-            <app-topartists class="w-[70%] p-5" *ngIf="artists;else chooseSection"/>
+            <section class="w-[70%] p-5 relative" *ngIf="artists;else chooseSection">
+                <app-topartists class="w-[100%] " />
+                <button class="absolute w-[10%]  top-2 right-2 rounded-xl flex justify-center items-center" (click)="back()">
+                    <span class="text-gray-700 hover:text-gray-600 bg-black/10 rounded-xl flex justify-center items-center text-center w-[100%]  material-symbols-outlined">
+                        arrow_back_ios
+                    </span>
+                </button>
+            </section>
+            
             <ng-template #chooseSection>
                 <div class="w-[70%] flex flex-col p-5 justify-center items-center gap-[20%]">
                     <button (click)="getArtists()" class="text-3xl bg-blue-700 w-[50%] h-[15%] rounded-lg font-semibold hover:bg-blue-600">SHOW MY FAVOURITES ARTISTS</button>
@@ -63,5 +71,8 @@ export class DashboardComponent{
         this.token=localStorage.getItem("accessToken")
         this.showProfile=true
         this.getInfo()
+    }
+    back(){
+        this.artists=null;
     }
 }
--- frontend/src/app/components/topartists.component.ts
+++ frontend/src/app/components/topartists.component.ts
@@ -8,7 +8,7 @@ import { DashboardComponent } from './dashboard.component';
     imports:[NgFor],
     template: `
     <section class="flex flex-col items-center w-[100%] h-[100%]">
-        <h2 class="text-5xl font-bold border-b-2 border-b-black">TopArtists</h2>
+        <h2 class="text-5xl font-bold border-b-2 border-b-black w-[100%]">TopArtists</h2>
         <ol class="list-decimal w-[80%]">
             <li class="text-xl w-[100%] bg-black/5 m-2 p-1" *ngFor="let artist of artists.items">
                 {{artist.name}}
